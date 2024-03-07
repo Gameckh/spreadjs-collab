a8b9f4784ddc0c4d934feac928cc6f3922699721
Gameckh <chenkaihua_kevin@foxmail.com>
2024-03-07 23:23:25 +0800

    后台协同模块采用本地spreadjs依赖（启动更迅速）

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ startServer(server);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, async () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`SpreadJS-Collab Server is running on http://localhost:${PORT}`);
   await initSpreadJS();
   console.log('完成 SpreadJS 初始化');
 });
